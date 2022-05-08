3417b5d8285b8dd8e142382f9fcf4d374856f800
brunodominguez1303 <bruno.dominguez1303@gmail.com>
2022-05-08 03:23:03 -0300

    show html on page

--- html.js
+++ html.js
@@ -3,7 +3,11 @@ const http = require('http');
 const host = 'localhost';
 const port = 8080;
 
-function requestListener(req, res) {}
+function requestListener(req, res) {
+	res.setHeader('Content-Type', 'text/html');
+	res.writeHead(200);
+	res.end('<html><body><h1>This is HTML</h1></body></html>');
+}
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
