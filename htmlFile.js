44ae4c9d59a674dce169b70c07dd197dbe3bf3de
brunodominguez1303 <bruno.dominguez1303@gmail.com>
2022-05-08 03:46:22 -0300

    html static file loading on page

--- htmlFile.js
+++ htmlFile.js
@@ -1,11 +1,25 @@
 const http = require('http');
+const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8080;
 
-function requestListener(req, res) {}
+let indexFile;
+
+function requestListener(req, res) {
+	res.setHeader('Content-Type', 'text/html');
+	res.writeHead(200);
+	res.end(indexFile);
+}
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-	console.log(`Server is running on http://${host}:${port}`);
+
+fs.readFile(__dirname + '/index.html').then(contents => {
+	indexFile = contents;
+	server.listen(port, host, () => {
+		console.log(`Server running on http://${host}:${port}`);
+	});
+}).catch(err => {
+	console.error(`Could not read index.html file: ${err}`);
+	process.exit(1);
 });
